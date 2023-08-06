2f10e02f856f3e6205fef25ad5181cb48747c495
Kenny Young <tabascq@hotmail.com>
2023-08-06 16:11:34 -0700

    Remove '?.' operator; older browsers don't like it

--- puzzlejs/puzzle.js
+++ puzzlejs/puzzle.js
@@ -723,13 +723,13 @@ function PuzzleEntry(p, index) {
         var acrosscluenumber = cell.getAttribute("data-across-cluenumber");
         var downcluenumber = cell.getAttribute("data-down-cluenumber");
         if (acrosscluenumber) {
-            this.container.querySelector("li[data-across-cluenumber='" + acrosscluenumber + "']")?.classList.remove("marked");
+            this.container.querySelectorAll("li[data-across-cluenumber='" + acrosscluenumber + "']").forEach(li => { li.classList.remove("marked"); });
             if (this.dx !== 0) {
                 this.table.querySelectorAll("td[data-across-cluenumber='" + acrosscluenumber + "']").forEach(td => { td.classList.remove("marked"); });
             }
         }
         if (downcluenumber) {
-            this.container.querySelector("li[data-down-cluenumber='" + downcluenumber + "']")?.classList.remove("marked");
+            this.container.querySelectorAll("li[data-down-cluenumber='" + downcluenumber + "']").forEach(li => { li.classList.remove("marked"); });
             if (this.dy !== 0) {
                 this.table.querySelectorAll("td[data-down-cluenumber='" + downcluenumber + "']").forEach(td => { td.classList.remove("marked"); });
             }
@@ -886,7 +886,7 @@ function PuzzleEntry(p, index) {
         copyTd.innerText = inputTd.innerText;
         // If the td has a "value", overwrite the innertext.
         const text = inputTd.querySelector('.text span');
-        if (text?.innerText) {
+        if (text && text.innerText) {
             copyTd.innerText = text.innerText;
         }
 
